2b5eb2542459f60c1f4a62e7a64a7fd9980e130a
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-06-30 15:26:38 -0700

    pc - add controller for /api/hello

--- src/test/java/edu/ucsb/cs156/demo/springreact01/ApplicationTests.java
+++ src/test/java/edu/ucsb/cs156/demo/springreact01/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package edu.ucsb.cs156.demo.springreact01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
